a010ec4e489a71068345499d41e5515ce87c55ef
tiagochiavericosta <tiagochiavericosta@hotmail.com>
2016-06-26 16:02:04 -0300

    Update consulta.php

--- consulta.php
+++ consulta.php
@@ -3,6 +3,7 @@
     $dta_atendimento = $_POST['dta_atendimento'];
     $valor1 = $_POST['valor1'];
     $valor2 = $_POST['valor2'];
+    $valor3 = $_POST['valor3'];
     
     // Testar conexão
     $con = pg_connecti("host=dcmshare.ffclrp.usp.br port=5432 dbname=secret user=ibm15g9 password=2130") or die ("Falha na conexão!".pg_last_error());
@@ -23,7 +24,7 @@
     
     // Mostra o preço de um pacote.
     pg_query($con, "BEGIN;");
-    pg_query($con, "SELECT * FROM mostra_atendimentos(data_atendimento);");
+    pg_query($con, "SELECT * FROM mostra_valor_pacote(data_atendimento);");
 
     $result = pg_query($con, "FETCH ALL FROM a;");
 
