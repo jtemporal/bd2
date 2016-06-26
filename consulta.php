08ab9397de483e5c594881ea4b3bbddc1bb91624
tiagochiavericosta <tiagochiavericosta@hotmail.com>
2016-06-26 16:00:23 -0300

    Update consulta.php

--- consulta.php
+++ consulta.php
@@ -1,25 +1,52 @@
 <?php
     
     $dta_atendimento = $_POST['dta_atendimento'];
-    $dta_nasc = $_POST['dta_nasc'];
-    
+    $valor1 = $_POST['valor1'];
+    $valor2 = $_POST['valor2'];
     
     // Testar conexão
-    $conex1 = pg_connecti("host=dcmshare.ffclrp.usp.br port=5432 dbname=secret user=ibm15g9 password=2130") or die ("Falha na conexão!".pg_last_error());
-    // escrever os valores na ordem dentro do parentesis
-    $sql_func = "INSERT INTO FUNCIONARIO VALUES()";
+    $con = pg_connecti("host=dcmshare.ffclrp.usp.br port=5432 dbname=secret user=ibm15g9 password=2130") or die ("Falha na conexão!".pg_last_error());
+   
+   
+   // Mostra atendimento em determinada data
+   
+    pg_query($con, "BEGIN;");
+    pg_query($con, "SELECT * FROM mostra_atendimentos(data_atendimento);");
 
-    function insert(){
-        global $sql_func, $conex1;
-        $insertion = pg_query($conex1, $sql_func) or die ("Falha na inserção dos dados!<br>".pg_last_error());
-        if ($insertion){
-            echo "Cadastro do novo Funcionário realizado com sucesso";
-        } else {
-            echo "Falha ao cadastrar novo Funcionário, entre em contato com o administrador";
-        }
-        insert($conex1)
+    $result = pg_query($con, "FETCH ALL FROM a;");
+
+    echo '<pre>';
+    print_r(pg_fetch_all($result));
+    echo '</pre>';
+
+    pg_query($con, "COMMIT;");
+    
+    // Mostra o preço de um pacote.
+    pg_query($con, "BEGIN;");
+    pg_query($con, "SELECT * FROM mostra_atendimentos(data_atendimento);");
+
+    $result = pg_query($con, "FETCH ALL FROM a;");
+
+    echo '<pre>';
+    print_r(pg_fetch_all($result));
+    echo '</pre>';
+
+    pg_query($con, "COMMIT;");
+    
+    // Mostra pacotes entre dois valores.
+    pg_query($con, "BEGIN;");
+    pg_query($con, "SELECT * FROM mostra_pacotes_entrevalores(valor1, valor2);");
+
+    $result3 = pg_query($con, "FETCH ALL FROM a;");
+
+    echo '<pre>';
+    print_r(pg_fetch_all($result3));
+    echo '</pre>';
+
+    pg_query($con, "COMMIT;");
+    
     }
-    pg_close($conex1);
+    pg_close($con);
 
 
 ?>
