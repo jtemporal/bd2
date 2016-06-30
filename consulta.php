3942a2184ffa258092f2a50d720f1accb571a087
sathyagq <sathyagq@gmail.com>
2016-06-30 11:05:10 -0300

    Update consulta.php

--- consulta.php
+++ consulta.php
@@ -12,7 +12,7 @@
        	<div class="register">
 			<?php  
 	
-				// Testar conexão
+				//Conexão com o banco
 			    $con = pg_connect("host=143.107.137.62
 					port=5432
 					dbname=clinestet_new
@@ -20,15 +20,15 @@
 					password=2130") 
 			    or die ("Falha na conexão!".pg_last_error());
 			    
-				// Testar se o post para data de atendimento é vazio
-				
+				// Testar se o $_POST para data de atendimento é vazio
 				if(! (empty($_POST['dta_atendimento']) ) )
 				{
 					$dta_atendimento = $_POST['dta_atendimento'];
-					// Mostra atendimento em determinada data
+					//Seleciona os atendimentos com data igual a recebida
 					$result1 = pg_exec($con, "SELECT * FROM atendimento WHERE data='$dta_atendimento'");
 					$tot_tuplas = pg_numrows($result1);
 					echo "Para a data $dta_atendimento tivemos os seguintes atentimentos<br><br>";
+					//mostra todos os atendimentos da data recebida
 					for($tupla=0;$tupla<$tot_tuplas;$tupla++){
 						$atendimento = pg_result($result1,$tupla,0);
 						$horario = pg_result($result1,$tupla,1);
@@ -41,15 +41,15 @@
 					}
 				}
 	
-				// Testar se o post para numero de pacote é vazio
-	
+				// Testar se o $_POST para numero de pacote é vazio
 				if(! (empty($_POST['nroPacote']) ) )
 				{
 					$nroPacote = $_POST['nroPacote'];
-					// Mostra atendimento em determinada data
-					 $result2 = pg_exec($con, "SELECT * FROM pacote WHERE codigo=$nroPacote"); 
+					//Selecionando infos do pacote com codigo igual ao recebido
+					$result2 = pg_exec($con, "SELECT * FROM pacote WHERE codigo=$nroPacote"); 
 					$tot_tuplas = pg_numrows($result2);
 					echo "As informações para o pacote $nroPacote são:<br><br>";
+					//mostra todas as infos do pacote
 					for($tupla=0;$tupla<$tot_tuplas;$tupla++){
 						$valor = pg_result($result2,$tupla,1);
 						$desc = pg_result($result2,$tupla,2);
@@ -59,7 +59,7 @@
 					}
 				}
 	
-				// fecha conexão
+				//fecha conexão com o banco
 				pg_close($con);
 	
 			?>
