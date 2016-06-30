549879a009da629997f63bb87339a76663da2ff0
beambrosio <beamb0694@gmail.com>
2016-06-30 10:56:43 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -1,38 +1,51 @@
-<?php
-    
-    $dta_admissao = $_POST['dta_admissao'];
-    $dta_demissao = $_POST['dta_demissao'];
-    $dta_nasc = $_POST['dta_nasc'];
-    $nro_mat = $_POST['nro_mat'];
-    $cod_sec = $_POST['cod_sec'];
-    $cidade = $_POST['cidade'];
-    $estado = $_POST['estado'];
-    $bairro = $_POST['bairro'];
-    $fone = $_POST['fone'];
-    $cargo = $_POST['cargo'];
-    $nome = $_POST['nome'];
-    $sexo = $_POST['sexo'];
-    $cep = $_POST['cep'];
-    $cpf = $_POST['cpf'];
-    $rua = $_POST['rua'];
-    $rg = $_POST['rg'];
-    
-    // testar campos obrigatorios
-    $conex1 = pg_connecti("host=HOST port=PORTA dbname=BANCO user=SECRET password=PASSWORD") or die ("Falha na conexão!".pg_last_error());
-    // escrever os valores na ordem dentro do parentesis
-    $sql_func = "INSERT INTO FUNCIONARIO VALUES()";
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Cadastro ClinEstet</title>
+        <link rel="stylesheet" href="style.css" type="text/css">
+    </head>
+    <body>
+        <div>
+            <h1>Cadastro efetuado com sucesso!</h1>
+        </div>
+        <div class="register">
+            <h2>É NOIS JOCA</h2>
 
-    function insert(){
-        global $sql_func, $conex1;
-        $insertion = pg_query($conex1, $sql_func) or die ("Falha na inserção dos dados!<br>".pg_last_error());
-        if ($insertion){
-            echo "Cadastro do novo Funcionário realizado com sucesso";
-        } else {
-            echo "Falha ao cadastrar novo Funcionário, entre em contato com o administrador";
-        }
-        insert($conex1)
-    }
-    pg_close($conex1);
+	<?php
+	    
+		$dta_admissao = $_POST['dta_admissao'];
+		$dta_demissao = $_POST['dta_demissao'];
+		$dta_nasc = $_POST['dta_nasc'];
+		$nro_mat = $_POST['nro_mat'];
+		$cod_sec = $_POST['cod_sec'];
+		$cidade = $_POST['cidade'];
+		$estado = $_POST['estado'];
+		$bairro = $_POST['bairro'];
+		$fone = $_POST['fone'];
+		$cargo = $_POST['cargo'];
+		$nome = $_POST['nome'];
+		$sexo = $_POST['sexo'];
+		$cep = $_POST['cep'];
+		$cpf = $_POST['cpf'];
+		$rua = $_POST['rua'];
+		$rg = $_POST['rg'];
 
+		//print_r($_POST);
+	    
+		$conex1 = pg_connect("host = 143.107.137.62  
+					port = 5432  
+					dbname = clinestet_new  
+					user = ibm15g9  
+					password = 2130")
+			or die ("Falha na conexão!".pg_last_error()); 
 
-?>
+		$result1 = pg_exec($conex1, "INSERT INTO funcionario
+						VALUES ('$cpf','$dta_nasc','$sexo','$nome','$nro_mat','$rg','$dta_admissao','$cargo','$rua','$bairro','$cep','$cidade','$estado','$fone','$cod_sec','$dta_demissao');");
+	
+		pg_close($conex1);
+	
+	?>
+	</div>
+	</body>
+</html>
