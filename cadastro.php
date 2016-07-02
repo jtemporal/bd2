ea206058ce244abc3207cc65d737751c66bd5b04
Jessica Temporal <jessicatemporal@gmail.com>
2016-07-02 17:49:30 -0300

    added styling to cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -4,16 +4,21 @@
         <meta charset="UTF-8">
         <title>Cadastro ClinEstet</title>
         <link rel="stylesheet" href="style.css" type="text/css">
+        <style type="text/css">
+            <!--
+            .style1 { color: #606060; }
+            -->
     </head>
     <body>
         <div>
-            <h1>Cadastro efetuado com sucesso!</h1>
-        </div>
-        <div class="register">
-            <h2>É NOIS JOCA</h2>
+            <table width="147" border="0" align="center">
+                <tr>
+                    <td width="141"><span class="style1"><a href="principal.html"><img src="12 copy.jpg" alt="" width="140" height="111"></a></span></td>
+                </tr>
+            </table>
+            <h1 class="style1">Cadastro efetuado com sucesso!</h1>
 
-	<?php
-	    
+    <?php
 		$dta_admissao = $_POST['dta_admissao'];
 		$dta_demissao = $_POST['dta_demissao'];
 		$dta_nasc = $_POST['dta_nasc'];
@@ -31,13 +36,11 @@
 		$rua = $_POST['rua'];
 		$rg = $_POST['rg'];
 
-		//print_r($_POST);
-	    
-		$conex1 = pg_connect("host = 143.107.137.62  
-							port = 5432  
-							dbname = clinestet_new  
-							user = ibm15g9  
-							password = 2130")
+		$conex1 = pg_connect("host =   
+							port = 
+							dbname = 
+							user = 
+							password = ")
 							or die ("Falha na conexão!".pg_last_error()); 
 
 		$result1 = pg_exec($conex1, "INSERT INTO funcionario
@@ -45,7 +48,8 @@
 	
 		pg_close($conex1);
 	
-	?>
+    ?>
+    <p>&nbsp;</p>
 	</div>
 	</body>
 </html>
